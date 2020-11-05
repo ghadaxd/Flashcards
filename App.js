41afb946d691b8946edfcd2039ccf2679707873a
Ghada Fahad <gasker@tetco.sa>
2020-11-05 15:33:52 +0300

    Add plus icon for add deck action

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Text } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./src/reducers";
 
-import { purple, white } from "./src/utils/colors";
+import { purple } from "./src/utils/colors";
 
 import Home from "./src/components/screens/Home";
 import AddDeck from "./src/components/screens/AddDeck";
@@ -44,10 +45,10 @@ export default function App() {
             options={({ navigation }) => ({
               headerRight: () => (
                 <TouchableOpacity
-                  style={{ width: "120%", right: "15%" }}
+                  style={{ width: "120%", right: "50%" }}
                   onPress={() => navigation.navigate("Add Deck")}
                 >
-                  <Text style={{ color: white }}>Add Deck</Text>
+                  <MaterialIcons name="add" size={24} color="white" />
                 </TouchableOpacity>
               ),
             })}
